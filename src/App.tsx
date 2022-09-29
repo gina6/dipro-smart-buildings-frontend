81cadbf203509b6c369303444ec2f66f235d8b80
gina6 <gina.fontana@stud.hslu.ch>
2022-09-29 11:59:00 +0200

    intall tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -7,9 +7,9 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
+        <h1 className="text-3xl font-bold underline">
+          Hi there :)
+        </h1>
         <a
           className="App-link"
           href="https://reactjs.org"
