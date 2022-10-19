c6d711b82bba28d6a72d4f9f05d9b394001f6a3a
gina6 <gina.fontana@stud.hslu.ch>
2022-10-19 14:23:48 +0200

    add Navigation Tab Indicator

--- src/components/MobileBottomNav.tsx
+++ src/components/MobileBottomNav.tsx
@@ -1,12 +1,13 @@
 import React from "react";
-import { BiBell, BiSearch, BiBuildings} from 'react-icons/bi';
+import { BiBell, BiSearch, BiBuildings } from 'react-icons/bi';
 import { Link } from "react-router-dom";
 import PlantSearch from "./UI/PlantSearch";
 import { useState } from "react";
 
 export default function MobileBottomNav() {
 
-  const [searchOpen, setSearchOpen] = useState(false)
+  const [searchOpen, setSearchOpen] = useState(false);
+  const [notificationNavIndicator, setNavIndicator] = useState(true);
 
   const toggleSearch = () => {
     setSearchOpen(!searchOpen);
@@ -21,18 +22,18 @@ export default function MobileBottomNav() {
             <BiSearch className="scale-150 text-white" onClick={toggleSearch} />
           </div>
         </div>
-
-        <div className="flex flex-row w-full h-14 justify-around items-center bg-gray-300">
-
-          <div>
-              <Link to="/"><BiBell className="scale-150" /></Link>
-
-          </div>
-          <div>
-              <Link to="/floors"><BiBuildings className="scale-150" /></Link>
-          </div>
-        </div >
+        <div className="flex flex-col w-full h-14">
+          {notificationNavIndicator ? <div className="flex self-start bg-gray-900 w-1/2 h-1"></div> : <div className="flex self-end bg-gray-900 w-1/2 h-1"></div>}
+          <div className="flex flex-row w-full h-14 items-center bg-gray-300">
+            <Link id="notificationTab" className="h-full w-1/2 flex justify-center items-center" to="/" onClick={() => setNavIndicator(true)}>
+              <BiBell className="scale-150" />
+            </Link>
+            <Link id="buildingTab" className="h-full w-1/2 flex justify-center items-center" to="/floors" onClick={() => setNavIndicator(false)}>
+              <BiBuildings className="scale-150" />
+            </Link>
+          </div >
+        </div>
       </div>
-    </div>
-    )
+    </div >
+  )
 }
