7fb41a2618081666475c30da5c448b6cb01a6ba2
gina6 <gina.fontana@stud.hslu.ch>
2022-10-20 12:01:13 +0200

    add water warning component

---
+++ src/components/UI/WarningWater.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { IoWaterOutline } from 'react-icons/io5';
+import { FiChevronDown, FiChevronUp } from 'react-icons/fi';
+import { useState } from "react";
+
+export default function WarningWater() {
+
+  const [detailsOpen, setDetailsOpen] = useState(false);
+
+  const toggleDetail = () => {
+    setDetailsOpen(!detailsOpen);
+  }
+
+
+  return (
+    <div className="flex flex-col p-3 my-5 bg-sky-200 rounded-lg">
+      <div className="flex items-center justify-evenly">
+        <IoWaterOutline className="scale-150" />
+        <div>
+          <h3>Wasserbestand kritisch</h3>
+          <p>vor 3 Tagen</p>
+        </div>
+        <div onClick={toggleDetail}>
+          {detailsOpen ? <FiChevronUp className="scale-150 ml-3" /> : <FiChevronDown className="scale-150 ml-3" />}
+
+        </div>
+      </div>
+      {detailsOpen && (
+        <div className="px-3 pt-3 text-sm">
+          <p>
+            Der Wasserbestand befindet sich unter dem kritischen Schwellwert.
+            Bitte das Reservoir bis zur Markierung mit Wasser auffüllen.
+          </p>
+        </div>
+      )}
+    </div>
+  )
+}
