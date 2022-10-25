3901092391ea8219eb860cecf8bce9c396add1cf
gina6 <gina.fontana@stud.hslu.ch>
2022-10-25 15:48:34 +0200

    update water warning component

--- src/components/UI/WarningWater.tsx
+++ src/components/UI/WarningWater.tsx
@@ -1,19 +1,11 @@
 import React from "react";
 import { IoWaterOutline } from 'react-icons/io5';
-import { FiChevronDown, FiChevronUp } from 'react-icons/fi';
-import { useState } from "react";
 
 interface WarningProps {
   date: string;
 }
 
 export default function WarningWater({ date }: WarningProps) {
-
-  const [detailsOpen, setDetailsOpen] = useState(false);
-
-  const toggleDetail = () => {
-    setDetailsOpen(!detailsOpen);
-  }
 
   return (
     <div className="flex flex-col p-3 my-5 bg-sky-200 rounded-lg">
@@ -23,19 +15,7 @@ export default function WarningWater({ date }: WarningProps) {
           <h3>Wasserbestand kritisch</h3>
           <p>{date}</p>
         </div>
-        <div onClick={toggleDetail}>
-          {detailsOpen ? <FiChevronUp className="scale-150 ml-3" /> : <FiChevronDown className="scale-150 ml-3" />}
-
-        </div>
       </div>
-      {detailsOpen && (
-        <div className="px-3 pt-3 text-sm">
-          <p>
-            Der Wasserbestand befindet sich unter dem kritischen Schwellwert.
-            Bitte das Reservoir bis zur Markierung mit Wasser auffüllen.
-          </p>
-        </div>
-      )}
     </div>
   )
 }
