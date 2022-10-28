bc7aacae610a1f43382125105bee74012854976e
gina6 <gina.fontana@stud.hslu.ch>
2022-10-28 11:15:00 +0200

    add colors

--- src/components/MobileBottomNav.tsx
+++ src/components/MobileBottomNav.tsx
@@ -19,14 +19,14 @@ export default function MobileBottomNav() {
         <PlantSearch closeSearch={toggleSearch}></PlantSearch>
       )}
       <div className="flex justify-center">
-        <div className="flex justify-center bottom-5 z-10 bg-gray-900 rounded-full fixed ">
+        <div className="flex justify-center bottom-5 z-10 bg-green rounded-full fixed ">
           <div className="p-6">
             <BiSearch className="scale-150 text-white" onClick={toggleSearch} />
           </div>
         </div>
         <div className="flex flex-col w-full h-14">
           {notificationNavIndicator ? <div className="flex self-start bg-gray-900 w-1/2 h-1"></div> : <div className="flex self-end bg-gray-900 w-1/2 h-1"></div>}
-          <div className="flex flex-row w-full h-14 items-center bg-gray-300">
+          <div className="flex flex-row w-full h-14 items-center bg-white">
             <Link id="notificationTab" className="h-full w-1/2 flex justify-center items-center" to="/" onClick={() => setNavIndicator(true)}>
               <BiBell className="scale-150" />
             </Link>
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,14 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}",],
   theme: {
     extend: {},
+    colors: {
+      green: '#37523B',
+      plantGreen: '#6D9C4E',
+      white: '#ffffff',
+      water: '#EC4634',
+      darkGrey: '#AFAFAF',
+      lightGrey: '#E5E5E5'
+    }
   },
   plugins: [],
 }
