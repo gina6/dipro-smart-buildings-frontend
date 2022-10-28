2903c72bea75b32449a719e28c53bea06ffd3792
patriciqa <64805575+patriciqa@users.noreply.github.com>
2022-10-28 15:49:48 +0200

    styling of notifications

--- src/components/pages/Notifications.tsx
+++ src/components/pages/Notifications.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import NotificationAlert from "../UI/NotificationAlert";
 import { useApiGet } from "../../hook/useApiHook";
 import { NotificationInterface } from "../../hook/dataInterfaces";
-
+import CheckIcon from "../../icons/Check.svg";
 export default function Notifications() {
   const notifications =
     useApiGet<NotificationInterface[]>("/notifications").data;
@@ -10,20 +10,36 @@ export default function Notifications() {
   return (
     <>
       <h1>Benachrichtigungen</h1>
-      <div className="md:flex md:gap-5">
-        {notifications?.map(
-          (notification: NotificationInterface, index: number) => (
-            <NotificationAlert
-              key={index}
-              roomLabel={notification.room}
-              roomID={notification.roomId}
-              plantID={notification.plantId}
-              plantLabel={notification.plant}
-              plantImg={notification.plantImage}
-            />
-          )
-        )}
-      </div>
+      {notifications && notifications?.length > 1 ? (
+        <>
+          <h3 className="text-green pb-2 text-base">
+            {notifications[0].floor}
+          </h3>
+          <div className="md:flex md:gap-5">
+            {notifications?.map(
+              (notification: NotificationInterface, index: number) => (
+                <NotificationAlert
+                  key={index}
+                  roomLabel={notification.room}
+                  roomID={notification.roomId}
+                  plantID={notification.plantId}
+                  plantLabel={notification.plant}
+                  plantImg={notification.plantImage}
+                />
+              )
+            )}
+          </div>
+        </>
+      ) : (
+        <div className="flex h-[80vh] justify-center items-center	flex-col">
+          <div className="">
+            <img src={CheckIcon} alt="Everything okay" />
+          </div>
+          <div className=" text-3xl text-green font-bold	pt-3">
+            Alles in Ordnung!
+          </div>
+        </div>
+      )}
     </>
   );
 }
