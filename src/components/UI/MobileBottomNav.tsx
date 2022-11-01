10237b5d3ada8d0c530e82928e2cf370cb547309
Gina Fontana <gina.fontana@bluewin.ch>
2022-11-01 16:13:51 +0100

    update mobile bottom nav styling

--- src/components/UI/MobileBottomNav.tsx
+++ src/components/UI/MobileBottomNav.tsx
@@ -1,41 +1,46 @@
 import React from "react";
-import { BiBell, BiSearch, BiBuildings } from 'react-icons/bi';
-import { Link } from "react-router-dom";
+import { ReactComponent as Pflanzensuche } from "../../icons/Planzensuche.svg";
+import { ReactComponent as Pflanzenlexikon } from "../../icons/Pflanzenlexikon.svg";
+import { ReactComponent as Notficationbell } from "../../icons/Notification-bell.svg";
+
 import PlantSearch from "./PlantSearch";
 import { useState } from "react";
+import MobileBottomNavButton from "./MobileBottomNavButton";
 
 export default function MobileBottomNav() {
-
   const [searchOpen, setSearchOpen] = useState(false);
-  const [notificationNavIndicator, setNavIndicator] = useState(true);
+  const [activeTab, setActiveTab] = useState(true);
 
   const toggleSearch = () => {
     setSearchOpen(!searchOpen);
-  }
+  };
 
   return (
-    <div className="bottom-0 fixed w-screen">
-      {searchOpen && (
-        <PlantSearch closeSearch={toggleSearch}></PlantSearch>
-      )}
-      <div className="flex justify-center">
-        <div className="flex justify-center bottom-5 z-10 bg-green rounded-full fixed ">
-          <div className="p-6">
-            <BiSearch className="scale-150 text-white" onClick={toggleSearch} />
+    <div className="bottom-0 fixed w-full">
+      {searchOpen && <PlantSearch closeSearch={toggleSearch}></PlantSearch>}
+      <div className="flex justify-center drop-shadow-md">
+        <div className="flex justify-center bottom-5 z-10 bg-green rounded-full fixed drop-shadow-md">
+          <div className="p-3">
+            <Pflanzensuche onClick={toggleSearch} />
           </div>
         </div>
-        <div className="flex flex-col w-full h-14">
-          {notificationNavIndicator ? <div className="flex self-start bg-gray-900 w-1/2 h-1"></div> : <div className="flex self-end bg-gray-900 w-1/2 h-1"></div>}
-          <div className="flex flex-row w-full h-14 items-center bg-white">
-            <Link id="notificationTab" className="h-full w-1/2 flex justify-center items-center" to="/" onClick={() => setNavIndicator(true)}>
-              <BiBell className="scale-150" />
-            </Link>
-            <Link id="buildingTab" className="h-full w-1/2 flex justify-center items-center" to="/floors/" onClick={() => setNavIndicator(false)}>
-              <BiBuildings className="scale-150" />
-            </Link>
-          </div >
+        <div className="flex flex-col w-full h-14 ">
+          <div className="flex flex-row w-full h-14 items-center bg-white rounded-t-2xl">
+            <MobileBottomNavButton
+              linkTo="/"
+              active={activeTab}
+              childIcon={<Notficationbell />}
+              setActiveTab={() => setActiveTab(true)}
+            />
+            <MobileBottomNavButton
+              linkTo="/floors/"
+              active={!activeTab}
+              childIcon={<Pflanzenlexikon />}
+              setActiveTab={() => setActiveTab(false)}
+            />
+          </div>
         </div>
       </div>
-    </div >
-  )
+    </div>
+  );
 }
