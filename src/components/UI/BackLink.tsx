f94818ca90abca8ec0385b8221553e4b20d16563
gina6 <gina.fontana@stud.hslu.ch>
2022-11-02 10:17:50 +0100

    make backlink fixed

--- src/components/UI/BackLink.tsx
+++ src/components/UI/BackLink.tsx
@@ -7,7 +7,7 @@ export default function BackLink() {
 
   return (
     <div
-      className="flex items-center mb-5 h-16 text-white bg-back-button bg-no-repeat"
+      className="flex fixed left-0 top-0 items-center mb-5 w-2/5 h-16 text-white bg-back-button bg-no-repeat"
       onClick={() => navigate(-1)}
     >
       <Chevron className="mx-3"></Chevron>
