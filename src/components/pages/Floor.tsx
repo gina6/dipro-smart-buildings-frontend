64996d74d795377417ecf4042d4c8ad6580b83b5
Maxframe <max.werner@stud.hslu.ch>
2022-11-03 12:04:35 +0100

    improved styling of floor page and added plantcount in floor page

--- src/components/pages/Floor.tsx
+++ src/components/pages/Floor.tsx
@@ -9,6 +9,16 @@ import Dropdown from "../UI/Dopdown";
 export default function Floor() {
   const [floorId, setFloorId] = useState("floor4");
   const floorData = useApiGet<FloorInterface>(`/floors/${floorId}`).data;
+  const plantCount = countPlants();
+
+  function countPlants () {
+    let count = 0;
+
+    floorData?.rooms.forEach((room) => {
+      count += room.plantCount;
+    });
+    return count;
+  }
 
   function changeFloorId(floorId: string) {
     setFloorId(floorId);
@@ -20,23 +30,25 @@ export default function Floor() {
         headerTitle={<Dropdown changeFloorId={changeFloorId}></Dropdown>}
         headerSubtitle={"Pflanzenlexikon"}
       />
-      <Container>
-        <div className="flex flex-col">
-          <div className="flex flex-row mt-10">
-            {floorData?.rooms
-              .filter((room) => room.plantCount !== 0)
-              .map((filteredRoom, index: number) => (
-                <RoomBox
-                  key={index}
-                  linkTo={`/rooms/${filteredRoom.roomId}`}
-                  roomLabel={filteredRoom.roomLabel}
-                  plantCount={filteredRoom.plantCount}
-                  plantImgs={filteredRoom.plantImages}
-                ></RoomBox>
-              ))}
+        <Container>
+          <div className="flex flex-col mb-28">
+            <p className="text-right p-5 uppercase">{plantCount} Pflanzen</p>
+
+            <div className="grid grid-flow-row grid-cols-2 gap-3 md:grid-cols-3 lg:grid-cols-4">
+              {floorData?.rooms
+                .filter((room) => room.plantCount !== 0)
+                .map((filteredRoom, index: number) => (
+                  <RoomBox
+                    key={index}
+                    linkTo={`/rooms/${filteredRoom.roomId}`}
+                    roomLabel={filteredRoom.roomLabel}
+                    plantCount={filteredRoom.plantCount}
+                    plantImgs={filteredRoom.plantImages}
+                  ></RoomBox>
+                ))}
+            </div>
           </div>
-        </div>
-      </Container>
+        </Container>
     </>
   );
 }
