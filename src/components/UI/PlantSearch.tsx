e92ed3d32b7e1da0925a61f4bec70f16e3143923
gina6 <gina.fontana@stud.hslu.ch>
2022-11-03 10:07:03 +0100

    replace icon library icons

--- src/components/UI/PlantSearch.tsx
+++ src/components/UI/PlantSearch.tsx
@@ -1,15 +1,14 @@
 import React, { useEffect, useRef, useState } from "react";
-import { RiCloseLine } from 'react-icons/ri';
+import { ReactComponent as CloseIcon } from "../../icons/Close-Icon.svg";
 import { generatePath, useNavigate } from "react-router-dom";
 import { useApiGet } from "../../hook/useApiHook";
-import { PlantInterface } from "../../hook/dataInterfaces"
+import { PlantInterface } from "../../hook/dataInterfaces";
 
 interface OverlayProps {
   closeSearch: () => void;
 }
 
 export default function PlantSearch({ closeSearch }: OverlayProps) {
-
   const [error, setError] = useState(false);
   const [route, setRoute] = useState<string | null>(null);
   const searchInput = useRef<HTMLInputElement>(null);
@@ -23,31 +22,38 @@ export default function PlantSearch({ closeSearch }: OverlayProps) {
   useEffect(() => {
     if (plantData.data) {
       setError(false);
-      navigate(generatePath("/rooms/:roomID/:plantID", {
-        roomID: plantData?.data.roomId,
-        plantID: plantData?.data.plantId,
-      }))
+      navigate(
+        generatePath("/rooms/:roomID/:plantID", {
+          roomID: plantData?.data.roomId,
+          plantID: plantData?.data.plantId,
+        })
+      );
       closeSearch();
     } else if (plantData.error) {
-      setError(true)
+      setError(true);
     }
   }, [plantData, navigate, closeSearch]);
-
 
   return (
     <div>
       <div className="flex flex-col items-center p-5 pb-10 bg-gray-100 rounded-t-lg mx-5">
         <div onClick={closeSearch} className="self-end scale-150">
-          <RiCloseLine></RiCloseLine>
+          <CloseIcon />
         </div>
-        <h3 className="pb-5">
-          ID eingeben
-        </h3>
-        <input ref={searchInput} type="text" name="searchInput" autoComplete="off" className="p-2 bg-transparent border-2 border-transparent border-b-gray-600 rounded-sm outline-none text-center" placeholder="plant000_00" />
-        {error && (<p className="text-red-700">Pflanze nicht gefunden!</p>)
-        }
-        <button onClick={searchPlantPage} className="mt-3">Suchen</button>
+        <h3 className="pb-5">ID eingeben</h3>
+        <input
+          ref={searchInput}
+          type="text"
+          name="searchInput"
+          autoComplete="off"
+          className="p-2 bg-transparent border-2 border-transparent border-b-gray-600 rounded-sm outline-none text-center"
+          placeholder="plant000_00"
+        />
+        {error && <p className="text-red-700">Pflanze nicht gefunden!</p>}
+        <button onClick={searchPlantPage} className="mt-3">
+          Suchen
+        </button>
       </div>
     </div>
-  )
+  );
 }
