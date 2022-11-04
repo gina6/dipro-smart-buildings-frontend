82b6f856d3c0437869bd21007b18059cc089e709
gina6 <gina.fontana@stud.hslu.ch>
2022-11-04 16:16:00 +0100

    put search on top of backlink

--- src/components/UI/PlantSearch.tsx
+++ src/components/UI/PlantSearch.tsx
@@ -57,7 +57,7 @@ export default function PlantSearch({ closeSearch }: OverlayProps) {
 
   return createPortal(
     <div
-      className="fixed top-0 bottom-0 left-0 right-0 bg-[black]/40 z-20"
+      className="fixed top-0 bottom-0 left-0 right-0 bg-[black]/40 z-40"
       onClick={closeSearch}
     >
       <div
